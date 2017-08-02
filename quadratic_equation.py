bb84db3b8ada7dd98a262c145a78cb25b6f6cbfd
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-02 18:26:58 +0300

    fixed complex value error

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,12 @@
-from math import sqrt
-
+import cmath
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    root1 = (-b - cmath.sqrt(discriminant)) / (2 * a)
+    root2 = (-b + cmath.sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
         return root1, None
+    elif discriminant < 0:
+        return None, None
     else:
         return root1, root2
